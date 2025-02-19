b25749d8e44d95ab76d0324b59227a9134dae216
Zelda <alumno@PC-V202-24.uacj.mx>
2025-02-19 10:40:30 -0700

    [PRIMER] commit para iniciar el repo

---
+++ mi_primera_app_storyboard/mi_primera_app_storyboard/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  mi_primera_app_storyboard
+//
+//  Created by alumno on 2/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var entrada_de_texto: UITextField!
+    
+    @IBOutlet weak var cajon_de_texto: UILabel!
+    
+    @IBAction func cuando_le_picamos(_ sender: Any) {
+        cajon_de_texto.text = entrada_de_texto.text
+        entrada_de_texto.text = ""
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Ya cargo la pantalla inicial")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
